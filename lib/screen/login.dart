e5a25a1d486c25fa0c44201c0a8e8de800ae2283
waranya3294 <106942821+waranya3294@users.noreply.github.com>
2024-03-23 22:55:46 +0700

    Add files via upload

---
+++ lib/screen/login.dart
@@ -0,0 +1,111 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:form_field_validator/form_field_validator.dart';
+import 'package:lab10/model/profile.dart';
+import 'package:lab10/screen/welcome.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final formKey = GlobalKey<FormState>();
+  Profile profile = new Profile();
+  final Future<FirebaseApp> firebase = Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyDsvh2gVSPukrjLwDs3WITV_HQ7hWbyG_M",
+      appId: "1:438109809165:android:b1b081513ab7289cfd4344",
+      messagingSenderId: "438109809165",
+      projectId: "lab9-245c9",
+    ),
+  );
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: firebase,
+        builder: (context, snapshot) {
+          if (snapshot.hasError)
+            return Scaffold(
+              appBar: AppBar(title: Text('Error')),
+              body: Center(child: Text('${snapshot.error}')),
+            );
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Scaffold(
+                appBar: AppBar(title: Text('ลงชื่อเข้าใช้')),
+                body: Container(
+                  padding: const EdgeInsets.all(20),
+                  child: Form(
+                      key: formKey,
+                      child: SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('อีเมล', style: TextStyle(fontSize: 20)),
+                            TextFormField(
+                              validator: MultiValidator([
+                                RequiredValidator(errorText: 'กรุณาป้อนอีเมล'),
+                                EmailValidator(
+                                    errorText: 'รูปแบบอีเมลไม่ถูกต้อง')
+                              ]),
+                              keyboardType: TextInputType.emailAddress,
+                              onSaved: (email) {
+                                profile.email = email;
+                              },
+                            ),
+                            SizedBox(height: 15),
+                            Text('รหัสผ่าน', style: TextStyle(fontSize: 20)),
+                            TextFormField(
+                                obscureText: true,
+                                validator: RequiredValidator(
+                                    errorText: 'กรุณาป้อนรหัสผ่าน'),
+                                onSaved: (password) {
+                                  profile.password = password;
+                                }),
+                            SizedBox(
+                                width: double.infinity,
+                                child: ElevatedButton.icon(
+                                    onPressed: () async {
+                                      if (formKey.currentState!.validate()) {
+                                        formKey.currentState!.save();
+                                        try {
+                                          await FirebaseAuth.instance
+                                              .signInWithEmailAndPassword(
+                                                  email:
+                                                      profile.email.toString(),
+                                                  password: profile.password
+                                                      .toString())
+                                              .then((value) {
+                                            formKey.currentState!.reset();
+                                            Navigator.pushReplacement(context,
+                                                MaterialPageRoute(
+                                                    builder: (context) {
+                                              return WelcomeScreen();
+                                            }));
+                                          });
+                                        } on FirebaseAuthException catch (e) {
+                                          Fluttertoast.showToast(
+                                              msg: e.message.toString());
+                                        }
+                                      }
+                                    },
+                                    icon: Icon(Icons.app_registration_rounded),
+                                    label: Text(
+                                      'ลงชื่อเข้าใช้',
+                                      style: TextStyle(fontSize: 20),
+                                    )))
+                          ],
+                        ),
+                      )),
+                ));
+          }
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        });
+  }
+}
---
+++ lib/screen/welcome.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:lab10/screen/home.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  final auth = FirebaseAuth.instance;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Welcome')),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Center(
+          child: Column(
+            children: [
+              Text(
+                'อีเมลของคุณคือ ' + auth.currentUser!.email.toString(),
+                style: TextStyle(fontSize: 20),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) {
+                      return HomeScreen();
+                    }));
+                  },
+                  child: Text('ออกจากระบบ'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
